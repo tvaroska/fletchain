3e0729e103c5fd3592ff48b6d259cb1b90bef532
Boris Tvaroska <boris@tvaroska.sk>
2024-01-05 19:11:40 +0000

    feat: readme file - initial docu

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fletchain
 Flet.dev chat component with Langchain integration
+
+## Install
+Package is not yet on PYPI, the only option is to install directly from github
+pip install git+https://github.com/tvaroska/fletchain.git
+
+## Ussage
+
+Example in [Demo/Simple](demo/simple.py)
---
+++ demo/simple.py
@@ -0,0 +1,58 @@
+# requires: langchain google-cloud-aiplatform
+
+import flet as ft
+
+from fleetchain import FletChain
+
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain.chat_models import ChatVertexAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory
+
+async def main(page: ft.Page):
+
+    model = ChatVertexAI()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful chatbot"),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{input}"),
+        ]
+    )
+    memory = ConversationBufferMemory(return_messages=True)
+    chain = (
+        RunnablePassthrough.assign(
+            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+        )
+        | prompt
+        | model
+    )
+
+
+    appbar_items = [
+        ft.PopupMenuItem(text="Login"),
+        ft.PopupMenuItem(),  # divider
+        ft.PopupMenuItem(text="Settings")
+    ]
+    appbar = ft.AppBar(
+        leading=ft.Icon(ft.icons.GRID_GOLDENRATIO_ROUNDED),
+        leading_width=40,
+        title=ft.Text("FletChain", text_align="start"),
+        center_title=False,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        actions=[
+            ft.Container(
+                content=ft.PopupMenuButton(
+                    items=appbar_items
+                ),
+                margin=ft.margin.only(left=50, right=25)
+            )
+        ],
+    )
+    page.appbar = appbar
+    page.vertical_alignment = ft.MainAxisAlignment.END
+
+
+    await page.add_async(FletChain(chain, memory))
+
+ft.app(target= main, view = ft.AppView.WEB_BROWSER)
