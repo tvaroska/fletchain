0a9d3ea61ed482fbdaa07259a80d46c3f97e6d43
Boris Tvaroska <boris@tvaroska.sk>
2024-09-21 15:35:15 +0000

    bug: overwrite of user entry with chat history

--- fleetchain/chain.py
+++ fleetchain/chain.py
@@ -1,4 +1,5 @@
 import uuid
+import logging
 
 import flet as ft
 from langchain_core.runnables import Runnable
@@ -8,6 +9,11 @@ from langchain_core.memory import BaseMemory
 from fleetchain.user import UserEntry, ChatMessage
 
 SESSION_ID = 'session_id'
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class FletChain(ft.Column):
     def __init__(self,
@@ -23,26 +29,38 @@ class FletChain(ft.Column):
         self.chain = chain
         self.memory = memory
         self.memory_chain = isinstance(self.chain, RunnableWithMessageHistory)
+
         self.user_initials = self.get_initials(user_name)
         self.user_color = user_color
         self.user_bg_color = user_bgcolor
         self.ai_initials = self.get_initials(ai_name)
         self.ai_color = ai_color
         self.ai_bgcolor = ai_bgcolor
+
         self.user_entry = UserEntry(self.user_initials, self.send_message_click)
+
         self.progress_ring = ft.ProgressRing(width=20, height=20, value=1, visible=False)
         self.progress_text = ft.Text('')
         self.progress_row = ft.Row(controls=[self.progress_ring, self.progress_text], expand=True)
-        self.messages = ft.ListView(
+
+        self.messages = ft.ListView(expand=True, spacing=10, auto_scroll=True)
+        self.history = ft.Container(
+            content=self.messages,
             expand=True,
-            spacing=10,
-            auto_scroll=True,
+            # border=ft.border.all(1, ft.colors.OUTLINE),
+            # border_radius=5,
+            padding=10,
         )
+
         self.controls = [
-            ft.Container(content=self.messages, expand=True),
-            ft.Container(content=self.progress_row, expand=True),
-            ft.Container(content=self.user_entry),
+            ft.Column([
+                self.history,
+                ft.Container(content=self.progress_row, height=30),
+                ft.Container(content=self.user_entry, height=50),
+            ], expand=True, spacing=10)
         ]
+
+        self.expand = True
 
     def get_initials(self, name: str) -> str:
         if len(name) <= 2:
@@ -72,9 +90,9 @@ class FletChain(ft.Column):
         else:
             response = self.chain.invoke(inputs)
         self.save_context(inputs, response.content)
-        self.messages.controls.append(ChatMessage(self.user_initials, self.user_entry.value))
+        self.messages.controls.insert(0, ChatMessage(self.ai_initials, response.content))
+        self.messages.controls.insert(0, ChatMessage(self.user_initials, self.user_entry.value))
         await self.user_entry.clear()
-        self.messages.controls.append(ChatMessage(self.ai_initials, response.content))
         self.progress_ring.visible = False
         self.progress_text.visible = False
         e.page.update()
@@ -82,3 +100,8 @@ class FletChain(ft.Column):
     def save_context(self, inputs, outputs):
         if not self.memory_chain and self.memory:
             self.memory.save_context(inputs, {'output': outputs})
+
+    def resize(self, e):
+        self.height = e.page.height
+        self.history.height = max(0, e.page.height - 100)  # Subtract height for progress row and user entry
+        self.update()
